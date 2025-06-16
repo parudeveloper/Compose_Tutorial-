b52cef5a61ad140c8d4924be31fc79fe4cb9117f
Paramesh <Paramesh.developer@gmail.com>
2025-06-16 06:37:10 +0530

    RememberCoroutineScope Side Effect

--- app/src/main/java/com/androidwithcomposetutorial/MainActivity.kt
+++ app/src/main/java/com/androidwithcomposetutorial/MainActivity.kt
@@ -21,7 +21,12 @@ import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.key
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -29,6 +34,8 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -68,7 +75,14 @@ fun MainScreen(mainActivityViewModel: MainActivityViewModel = viewModel()) {
         }
     }
 }*/
-
+suspend fun startCounterValue(increaseCounter : () ->Unit){
+    for (i in 1..100){
+        delay(1000)
+        increaseCounter()
+    }
+}
+/*
+// Derived State Of Side Effect
 @Composable
 fun MainScreen(mainActivityViewModel: MainActivityViewModel = viewModel()) {
     val counterBackgroundColour by remember(mainActivityViewModel.counterNumber) {
@@ -100,13 +114,59 @@ fun MainScreen(mainActivityViewModel: MainActivityViewModel = viewModel()) {
                 modifier = Modifier
                     .background(counterBackgroundColour)
                     .width(50.dp)
-                    .height(50.dp).padding(15.dp),
+                    .height(50.dp)
+                    .padding(15.dp),
                 textAlign = TextAlign.Center
-                )
+            )
 
             Button(onClick = { mainActivityViewModel.incrementCounterNumber() }) {
                 Text(text = "Increment")
             }
+        }
+    }
+}*/
+@Composable
+fun MainScreen(mainActivityViewModel: MainActivityViewModel = viewModel()) {
+    val counterBackgroundColour by remember(mainActivityViewModel.counterNumber) {
+        derivedStateOf {
+            if (mainActivityViewModel.counterNumber % 2 == 0) Color.Red else Color.Green
+        }
+    }
+    val coroutineScope = rememberCoroutineScope()
+    var counter by remember{ mutableIntStateOf(0) }
+
+    val incrementCounter ={
+        counter++
+    }
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceAround,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(2.dp)
+        ) {
+            Button(
+                onClick = { coroutineScope.launch {
+                    startCounterValue {incrementCounter() }
+                } },
+                modifier = Modifier.padding(5.dp)
+            ) {
+                Text(text = "Start Counter")
+            }
+            Text(
+                text = "$counter",
+                modifier = Modifier
+                    .background(counterBackgroundColour)
+                    .width(50.dp)
+                    .height(50.dp)
+                    .padding(15.dp),
+                textAlign = TextAlign.Center
+            )
         }
     }
 }
